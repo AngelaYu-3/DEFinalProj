51b1032506fb55f64667f59c4ab30eb0da13d5f2
Angela Yu <angelayu@Angelas-MacBook-Pro.local>
2022-01-07 13:35:43 -0800

    fixed reset screen

--- DEFinalProject.ino
+++ DEFinalProject.ino
@@ -1,8 +1,6 @@
 /*
  * TODO:
- *   (1) fix code for resetting screen from completed to start to back (clearSwitchState)
- *   (2) code for how to get back to original screen--ie reset terminated
- *   (3) replace uno with nano
+ *   (1) replace uno with nano
  */
 
 #include <Adafruit_GFX.h>    
@@ -14,13 +12,13 @@
 String goal = "WORKOUT";
 
 //led pin inputs
-const int in1 = 2, in2 = 4, in3 = 6, in4 = 8, in5 = 10, in6 = 12;
+const int in1 = 2, in2 = 3, in3 = 4, in4 = 5, in5 = 6, in6 = 7, in7 = 8;
 
 //led pin outputs
-const int out1 = 3, out2 = 5, out3 = 7, out4 = 9, out5 = 11, out6 = 13;
+//const int out1 = 3, out2 = 5, out3 = 7, out4 = 9, out5 = 11, out6 = 13;
 
 //clear switch pin
-const int clearLED = 6; 
+const int clearLED = 9; 
 
 //led states
 int signals[7] = {1, 1, 1, 1, 1, 1, 1};
@@ -31,12 +29,13 @@ int dayY[7] = {35, 35, 35, 70, 70, 70, 105};
 
 //other variables
 int on = 1;
-bool isReset = false;
-int clearSwitch = 0;
+bool isReset1 = false;
+bool isReset2 = false;
+int clearSwitch = 1;
 
-#define TFT_CS 10
-#define TFT_RST 9
-#define TFT_DC 8
+#define TFT_CS 12
+#define TFT_RST 1
+#define TFT_DC 0
 #define red 0x861F
 #define green 0xCFF6
 
@@ -45,11 +44,13 @@ Turtle t = Turtle(&tft);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   pinMode(in1, INPUT);
   pinMode(in2, INPUT);
-  pinMode(out1, OUTPUT);
-  pinMode(out2, OUTPUT);
+  pinMode(in3, INPUT);
+  pinMode(in4, INPUT);
+  pinMode(in5, INPUT);
+  pinMode(in6, INPUT);
+  pinMode(in7, INPUT);
 
   pinMode(clearLED, INPUT);
 
@@ -67,8 +68,6 @@ void setup() {
 }
 
 void clearAll(){
-  digitalWrite(out1, 0);
-  digitalWrite(out2, 0); 
 
   for(int i = 0; i < 7; i++){
     if(isSwitched[i]){
@@ -106,13 +105,13 @@ void day(int day, uint16_t color){
 }
 
 bool isAllDone(){
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < 7; i++){
     if(!isDone[i]) return false;
   }return true;
 }
 
 void resetIsDone(){
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < 7; i++){
     isDone[i] = false;
   }
 }
@@ -125,8 +124,6 @@ void switchClearState(){
 void resetOgScreen(){
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST77XX_BLACK);
-  Serial.println(tft.width());
-  Serial.println(tft.height());
   tft.setRotation(-45);
   goalLine(goal);
 
@@ -147,17 +144,17 @@ void loop() {
     }
     else{
 
-      for(int i = 1; i < 3; i++){
-        if(digitalRead(i * 2) == signals[i - 1]){
-          digitalWrite((i * 2) + 1, HIGH);
-          day(i - 1, green);
-          isSwitched[i - 1] = true;
-          isDone[i - 1] = true;
+      for(int i = 2; i < 9; i++){
+        if(digitalRead(i) == signals[i - 2]){
+          //digitalWrite((i * 2) + 1, HIGH);
+          day(i - 2, green);
+          isSwitched[i - 2] = true;
+          isDone[i - 2] = true;
         }
         else{
-          digitalWrite((i*2) + 1, LOW);
-          day(i - 1, red);
-          isDone[i - 1] = false;
+          //digitalWrite((i*2) + 1, LOW);
+          day(i - 2, red);
+          isDone[i - 2] = false;
         }
       }
   
@@ -165,20 +162,20 @@ void loop() {
   }
   
   else if(isAllDone()){
-    switchClearState();
-    int redo = clearSwitch;
     
-    if(!isReset){
+    if(!isReset1){
       tft.fillScreen(ST77XX_BLACK); 
       goalLine(goal);
-      t.star(5, 30, 36); 
-      isReset = true;
-    }
-
-    if(digitalRead(clearLED) == redo){
+      t.moveTo(55, 40);
+      t.setPenColor("cyan");
+      t.star(5, 40, 45); 
+      isReset1 = true;
+      
+    }else if (digitalRead(clearLED) == clearSwitch){
       resetOgScreen();
       resetIsDone();
-      isReset = false;
+      switchClearState();
+      isReset1 = false;
     }
     
   }
